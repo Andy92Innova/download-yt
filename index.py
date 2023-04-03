739a100aeb48d30c47928646de3c85030d134c6a
AndyDar92 <andydar20@gmail.com>
2023-04-03 13:40:50 -0500

    final

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from http.client import HTTPException
+from flask import Flask, render_template, request, jsonify, send_file
 from pytube import YouTube
 import os
 
@@ -10,55 +11,75 @@ def home():
 
 @app.route('/getFilters', methods=["POST"])
 def getFilters():
-    url = request.json['url']
+    try:
+        url = request.json['url']
 
-    yt = YouTube(url)
-    # Obtener una lista de streams
-    streams = yt.streams.filter(progressive=True)
-    # Obtener una lista de diccionarios con solo los campos 'itag' y 'mime_type'
-    result = [{
-        'itag': stream.itag, 
-        'mime_type': stream.mime_type,
-        'type':stream.type,
-        'subtype':stream.subtype,
-        'res':stream.resolution,
-        'abr':stream.abr,
-        'desc':yt.title
-        } for stream in streams]
+        yt = YouTube(url)
+        # Obtener una lista de streams
+        streams = yt.streams.filter(progressive=True)
+        # Obtener una lista de diccionarios con solo los campos 'itag' y 'mime_type'
+        result = [{
+            'itag': stream.itag, 
+            'mime_type': stream.mime_type,
+            'type':stream.type,
+            'subtype':stream.subtype,
+            'res':stream.resolution,
+            'abr':stream.abr,
+            'desc':yt.title
+            } for stream in streams]
 
-    streams2 = yt.streams.filter(only_audio=True)
-    # Obtener una lista de diccionarios con solo los campos 'itag' y 'mime_type'
-    result2 = [{
-        'itag': stream.itag, 
-        'mime_type': stream.mime_type,
-        'type':stream.type,
-        'subtype':stream.subtype,
-        'res':stream.resolution,
-        'abr':stream.abr,
-        'desc':yt.title
-        } for stream in streams2]
+        streams2 = yt.streams.filter(only_audio=True)
+        # Obtener una lista de diccionarios con solo los campos 'itag' y 'mime_type'
+        result2 = [{
+            'itag': stream.itag, 
+            'mime_type': stream.mime_type,
+            'type':stream.type,
+            'subtype':stream.subtype,
+            'res':stream.resolution,
+            'abr':stream.abr,
+            'desc':yt.title
+            } for stream in streams2]
 
-    result3 = result + result2
+        result3 = result + result2
 
-    # Imprimir los atributos de la primera transmisión
-    # result = dir(streams[0])
-    return jsonify({'data':result3})
+        # Imprimir los atributos de la primera transmisión
+        # result = dir(streams[0])
+        return jsonify({'data':result3})
+    
+    except Exception as e:
+        
+        # Si se produce un error, lanzamos una excepción HTTPException
+        raise HTTPException(status_code=500, description="No se pudo obtener los formatos")
+
+# Este manejador de errores captura todas las excepciones HTTPException
+# y las envía como una respuesta JSON con el código de estado y el mensaje de error
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = jsonify(error=str(e))
+    response.status_code = e.status_code
+    return response
+
+
 
 @app.route('/download',methods=["POST"])
-def getVideo():
-    path = 'videos/'
+def downloadObjeto():
+    try:
+        path = 'videos/'
+        url = request.json['url']
+        itag = request.json['itag']
+
+        yt = YouTube(url)
+        stream = yt.streams.get_by_itag(itag)
+        objeto = stream.download(path)
+
+        return send_file(objeto, as_attachment=True)
+    except Exception as e:
+        # Si se produce un error, lanzamos una excepción HTTPException
+        raise HTTPException(status_code=500, description="No se pudo descargar el video")
     
-    url = request.json['url']
-    itag = request.json['itag']
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    yt = YouTube(url)
-    stream = yt.streams.get_by_itag(itag)
-    result = stream.download(path).includes_audio_track()
-
-    return result
+@app.route('/helppay')
+def helppay():
+    return render_template('donar.html')
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,26 @@
+const inputUrl = document.getElementById('inputVideoUrl');
+const selectFormat = document.getElementById('selectFormat');
 const btnFilters = document.getElementById('btnGetFilters');
 const btnDownload = document.getElementById('btnDownload');
+const listError = document.getElementById('error');
+const loading = document.getElementById('loading');
 
 let  inputVideoUrl = '';
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Aquí puedes agregar el código que deseas ejecutar cuando la página esté lista.
+    btnDownload.setAttribute('hidden', true);
+    listError.setAttribute('hidden', true);
+    selectFormat.setAttribute('disabled', true);
+    loading.setAttribute('hidden', true);
+
+  });
+  
+
 btnFilters.addEventListener('click', ()=>{
-    inputVideoUrl = document.getElementById('inputVideoUrl').value;
+    inputVideoUrl = inputUrl.value;
+
+    loading.removeAttribute('hidden');
 
     fetch('/getFilters', {
         method:'POST',
@@ -19,9 +35,9 @@ btnFilters.addEventListener('click', ()=>{
     .then(data =>{
         let result = JSON.parse(data);
 
-        console.log('datos',result.data);
+        loading.setAttribute('hidden', true);
 
-        const selectFormat = document.getElementById('selectFormat')
+        console.log('datos',result.data);
 
         selectFormat.innerHTML='';
 
@@ -32,10 +48,10 @@ btnFilters.addEventListener('click', ()=>{
             let texto = ''
 
             if(element.type=="audio"){
-                texto = `${element.type} | ${element.desc} | ${element.mime_type} | ${element.abr}`
+                texto = `${element.type} || ${element.subtype} || ${element.desc} || ${element.mime_type} || ${element.abr}`
             }
             else{
-                texto = `${element.type} | ${element.desc} | ${element.mime_type} | ${element.res}`
+                texto = `${element.type} || ${element.subtype} || ${element.desc} || ${element.mime_type} || ${element.res}`
             }
 
             option.innerText = texto;
@@ -43,23 +59,24 @@ btnFilters.addEventListener('click', ()=>{
             selectFormat.appendChild(option);
         });
 
-        /*
-        {
-            "itag": 139,
-            "mime_type": "audio/mp4",
-            "res": null,
-            "subtype": "mp4",
-            "type": "audio"
-            },
-        */
-
+        btnDownload.removeAttribute('hidden');
+        selectFormat.removeAttribute('disabled');
+       
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+        console.error(error)
+        btnDownload.setAttribute('hidden', true);
+        selectFormat.setAttribute('disabled', 'disabled');
+        loading.setAttribute('hidden', true);
+        listError.removeAttribute('hidden').text = error;
+    });
 });
 
 
 btnDownload.addEventListener('click', ()=>{
-    const itag = document.getElementById('selectFormat').value;
+    const itag = selectFormat.value;
+
+    loading.removeAttribute('hidden');
 
     fetch('/download',{
         method:'POST',
@@ -71,7 +88,30 @@ btnDownload.addEventListener('click', ()=>{
             itag
         })
     })
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    .then(response => response.blob())
+    .then(data =>{
+        const url = window.URL.createObjectURL(data);
+        
+        let option = selectFormat.options[selectFormat.selectedIndex].text;
+        let parts = option.split('||')
+
+        const subtype = parts[1].trim();
+        const name = parts[2].trim();
+
+        let a = document.createElement('a')
+        a.href = url;
+        a.download = `${name}.${subtype}`;
+
+        a.click();
+        
+        loading.setAttribute('hidden', true);
+
+        window.URL.revokeObjectURL(url);
+    })
+    .catch(error =>
+        {
+            console.error(error)
+            loading.setAttribute('hidden', true);
+            listError.removeAttribute('hidden').text = error;
+        });
 });
